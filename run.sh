bc144a36b7ed87b581bbfac88aa97a4a1a604092
Mattias <mattiaslovkvist@gmail.com>
2016-10-25 16:04:38 +0200

    Added HUGO_PORT and HUGO_BIND

--- run.sh
+++ run.sh
@@ -7,6 +7,8 @@ echo "HUGO_REFRESH_TIME:" $HUGO_REFRESH_TIME
 echo "HUGO_THEME:" $HUGO_THEME
 echo "HUGO_BASEURL" $HUGO_BASEURL
 echo "HUGO_APPENDPORT" $HUGO_APPENDPORT
+echo "HUGO_PORT" $HUGO_PORT
+echo "HUGO_BIND" $HUGO_BIND
 
 HUGO=/usr/bin/hugo
 
@@ -14,10 +16,10 @@ while [ true ]
 do
     if [[ $HUGO_WATCH != 'false' ]]; then
 	    echo "Watching..."
-        $HUGO server --watch=true --source="/src" --theme="$HUGO_THEME" --destination="/output" --baseUrl="$HUGO_BASEURL" --appendPort="$HUGO_APPENDPORT" || exit 1
+        $HUGO server --watch=true --source="/src" --theme="$HUGO_THEME" --destination="/output" --baseUrl="$HUGO_BASEURL" --port="$HUGO_PORT" --appendPort="$HUGO_APPENDPORT" --bind="$HUGO_BIND" || exit 1
     else
 	    echo "Building one time..."
-        $HUGO --source="/src" --theme="$HUGO_THEME" --destination="/output" --baseUrl="$HUGO_BASEURL" --appendPort="$HUGO_APPENDPORT" || exit 1
+        $HUGO --source="/src" --theme="$HUGO_THEME" --destination="/output" --baseUrl="$HUGO_BASEURL" --port="$HUGO_PORT" --appendPort="$HUGO_APPENDPORT" || exit 1
     fi
 
     if [[ $HUGO_REFRESH_TIME == -1 ]]; then
