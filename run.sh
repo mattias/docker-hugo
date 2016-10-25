bc5790e6e3d16beb477fe679295f30315a1db594
Mattias <mattiaslovkvist@gmail.com>
2016-10-25 13:42:11 +0200

    Added HUGO_APPENDPORT

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ echo "HUGO_WATCH:" $WATCH
 echo "HUGO_REFRESH_TIME:" $HUGO_REFRESH_TIME
 echo "HUGO_THEME:" $HUGO_THEME
 echo "HUGO_BASEURL" $HUGO_BASEURL
+echo "HUGO_APPENDPORT" $HUGO_APPENDPORT
 
 HUGO=/usr/bin/hugo
 
@@ -13,10 +14,10 @@ while [ true ]
 do
     if [[ $HUGO_WATCH != 'false' ]]; then
 	    echo "Watching..."
-        $HUGO server --watch=true --source="/src" --theme="$HUGO_THEME" --destination="/output" --baseUrl="$HUGO_BASEURL" || exit 1
+        $HUGO server --watch=true --source="/src" --theme="$HUGO_THEME" --destination="/output" --baseUrl="$HUGO_BASEURL" --appendPort="$HUGO_APPENDPORT" || exit 1
     else
 	    echo "Building one time..."
-        $HUGO --source="/src" --theme="$HUGO_THEME" --destination="/output" --baseUrl="$HUGO_BASEURL" || exit 1
+        $HUGO --source="/src" --theme="$HUGO_THEME" --destination="/output" --baseUrl="$HUGO_BASEURL" --appendPort="$HUGO_APPENDPORT" || exit 1
     fi
 
     if [[ $HUGO_REFRESH_TIME == -1 ]]; then
@@ -25,4 +26,3 @@ do
     echo "Sleeping for $HUGO_REFRESH_TIME seconds..."
     sleep $SLEEP
 done
-
